4747daa91a03e1a1864db66f676aa30d58f1ca66
ErickPower <epower1996@gmail.com>
2019-10-24 22:01:55 -0600

    Tentative final version.

--- src/Driver.java
+++ src/Driver.java
@@ -11,37 +11,37 @@ public class Driver {
         
         //new Inspector().inspect(new ClassA(), false);
         
-//        runTest("script1.txt", new ClassA(), rec);
-//        runTest("script2.txt", new ClassA(12), rec);
+        runTest("script1.txt", new ClassA(), rec);
+        runTest("script2.txt", new ClassA(12), rec);
         runTest("script3.txt", new ClassB(), rec);
-//        runTest("script4.txt", new ClassD(32), rec);
-//        runTest("script5.txt", new ClassD(), rec);
-//        runTest("script6.txt", new ClassB[12], rec);
-//        runTest("script7.txt", new ClassB[12][12], rec);
-//        runTest("script8.txt", "Test String", rec);
+        runTest("script4.txt", new ClassD(32), rec);
+        runTest("script5.txt", new ClassD(), rec);
+        runTest("script6.txt", new ClassB[12], rec);
+        runTest("script7.txt", new ClassB[12][12], rec);
+        runTest("script8.txt", "Test String", rec);
     }
 
     public static void runTest(String filename, Object testObj, boolean recursive) {
         try {
-//            PrintStream old = System.out;
-//            File file = new File(filename);
-//            FileOutputStream fos = new FileOutputStream(file);
-//            PrintStream ps = new PrintStream(fos);
-//            System.setOut(ps);
+            PrintStream old = System.out;
+            File file = new File(filename);
+            FileOutputStream fos = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(fos);
+            System.setOut(ps);
             System.out.println("======================================================");
             System.out.println("Filename: " + filename);
             System.out.println("Running Test: " + testObj);
             System.out.println("Recursive: " + recursive);
             new Inspector().inspect(testObj, recursive);
             System.out.println("======================================================");
-//            ps.flush();
-//            fos.flush();
-//            ps.close();
-//            fos.close();
-//            System.setOut(old);
-        } /*catch (IOException ioe) {
+            ps.flush();
+            fos.flush();
+            ps.close();
+            fos.close();
+            System.setOut(old);
+        } catch (IOException ioe) {
             System.err.println("Unable to open file: " + filename);
-        }*/ catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("Unable to compleatly run test: " + testObj);
             e.printStackTrace();
         }
